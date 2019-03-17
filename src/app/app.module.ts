4edaae138fbf58024506fcbe3588d54ddb89c5b3
siwat jomewattana <s1104300051612@gmail.com>
2019-03-17 14:22:35 +0700

    สร้างลิงก์ไปหน้า About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about'; //1.ทุกครั้งที่สร้างเพจต้องสร้าง import
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage //2.ทุกครั้งที่สร้างเพจต้องสร้าง declarations
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage //3.ทุกครั้งที่สร้างเพจต้องสร้าง entryComponents
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about'; // ถ้าจะให้ลิงก์ไปหน้าอื่นต้อง import หน้านั้นเข้ามาด้วย
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoAbout(){
+    this.navCtrl.push(AboutPage); //ให้โหลดหน้า AboutPage ลงในที่นี้
   }
 
 }
